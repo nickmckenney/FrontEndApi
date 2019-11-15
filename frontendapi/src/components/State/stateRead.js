d70b0fcb7deca1d9cc8060085071dd252441aa08
NickMckenney <mckenney2001@gmail.com>
2019-11-15 10:55:17 -0500

    state is working in the console.log/ all states

--- frontendapi/src/components/State/stateRead.js
+++ frontendapi/src/components/State/stateRead.js
@@ -1,14 +1,38 @@
 import React, { Component } from "react";
 import "../../App.css";
-import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
-
-class StateRead extends Component {
+class stateRead extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      state: ""
+    };
+    this.getState = this.getState.bind(this);
+  }
+  getState() {
+    fetch("https://governmentfundingapi.herokuapp.com/state/", {
+      headers: {
+        Accept: "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+        this.setState({ state: res.state });
+        // console.log(this.state);
+      });
+  }
+  componentDidMount() {
+    this.getState();
+  }
   render() {
     return (
-      <div className="stateRead">
-        <h1 className="intro-stateRead-page">aaaaa</h1>
+      <div className="App">
+        <h1 className="App-header">states</h1>
+
+        <button onClick={this.getState}>New state</button>
       </div>
     );
   }
 }
-export default StateRead;
+
+export default stateRead;
