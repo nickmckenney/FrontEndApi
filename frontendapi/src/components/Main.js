b976fe906034be6fa1a13fcc5cb974fbb5d9a6d9
NickMckenney <mckenney2001@gmail.com>
2019-11-15 10:03:18 -0500

    added a stateCreate file for part of CRUD in state

--- frontendapi/src/components/Main.js
+++ frontendapi/src/components/Main.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./Home.js";
 import State from "./State.js";
+import StateCreate from "./State/stateCreate";
 
 const Main = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/state" component={State} />
+    <Route exact path="/create" component={StateCreate} />
   </Switch>
 );
 
---
+++ frontendapi/src/components/State/stateCreate.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "../../App.css";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+
+class StateCreate extends Component {
+  render() {
+    return (
+      <div className="stateCreate">
+        <h1 className="intro-stateCreate-page"></h1>
+      </div>
+    );
+  }
+}
+export default StateCreate;
