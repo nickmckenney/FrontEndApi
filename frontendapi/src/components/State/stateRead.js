e4238bc0f1c3347b4977f337cad31d894a527bf0
NickMckenney <mckenney2001@gmail.com>
2019-11-15 13:13:36 -0500

    The text for all states is displayign

--- frontendapi/src/components/State/stateRead.js
+++ frontendapi/src/components/State/stateRead.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import "../../App.css";
-// let states;
+import { Route, Link } from "react-router-dom";
 class stateRead extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      state: ""
+      state: []
     };
     this.getState = this.getState.bind(this);
   }
@@ -18,21 +18,31 @@ class stateRead extends Component {
       .then(res => res.json())
       .then(res => {
         console.log(res);
-        this.setState({ state: res.state });
+        this.setState({ state: res });
         console.log(this.state);
-        this.state = this.state;
       });
   }
-  componentWillUnmount() {
+  componentDidMount() {
     this.getState();
   }
   render() {
+    // let states = this.state.map(index => {
+    //   index.name;
+    // });
+    console.log(this.state);
+    let searchArray = this.state.state.map(
+      input => input.name + " ----------" + input.amount
+    );
     return (
       <div className="App">
         <h1 className="App-header">states</h1>
-
-        <button onClick={this.getState}>New state</button>
-        {/* <h1>{this.state}</h1> */}
+        <Link to="/read">
+          {/* <button onClick={this.getState}>New state</button> */}
+          <div className="statesPrinted">
+            <h1 className="stateFont">{searchArray}</h1>
+          </div>
+        </Link>
+        <Link to="/read/:state"></Link>
       </div>
     );
   }
