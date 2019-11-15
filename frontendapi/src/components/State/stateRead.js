9392e294a9482f55f3e1f8095cbb5c5c5d14873a
NickMckenney <mckenney2001@gmail.com>
2019-11-15 13:56:09 -0500

    I changed the text to display in lines

--- frontendapi/src/components/State/stateRead.js
+++ frontendapi/src/components/State/stateRead.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import "../../App.css";
+import { TableHeader, DataTable } from "react-mdl";
+
 import { Route, Link } from "react-router-dom";
 class stateRead extends Component {
   constructor(props) {
@@ -26,22 +28,37 @@ class stateRead extends Component {
     this.getState();
   }
   render() {
-    // let states = this.state.map(index => {
-    //   index.name;
-    // });
     console.log(this.state);
-    let searchArray = this.state.state.map(
-      input => input.name + " ----------" + input.amount
-    );
+    let searchArray = this.state.state.map(input => (
+      <p>
+        {input.name} + " " + {input.amount}
+      </p>
+    ));
     return (
       <div className="App">
         <h1 className="App-header">states</h1>
         <Link to="/read">
-          {/* <button onClick={this.getState}>New state</button> */}
           <div className="statesPrinted">
             <h1 className="stateFont">{searchArray}</h1>
+
+            {/* <DataTable
+              shadow={2}
+              rows={[{ state: input.name, fundingAmount: input.amount }]}
+            >
+              <TableHeader name="state" tooltip="state name">
+                State Names
+              </TableHeader>
+              <TableHeader
+                numeric
+                name="fundingAmount"
+                tooltip="Number of states"
+              >
+                unding Amount
+              </TableHeader>
+            </DataTable> */}
           </div>
         </Link>
+
         <Link to="/read/:state"></Link>
       </div>
     );
@@ -49,54 +66,3 @@ class stateRead extends Component {
 }
 
 export default stateRead;
-
-// import React from "react";
-// import Row from "../components/Row";
-
-// class Table extends React.Component {
-
-// constructor() {
-//     super();
-//     this.state = {
-//         users: [],
-//     };
-//     this.createNewUser = this.createNewUser.bind(this);
-//     this.deleteExistingUser = this.deleteExistingUser.bind(this);
-// }
-
-// componentDidMount() {
-//     console.log("Component did mount!");
-//     fetch("http://localhost:8080/users")
-//         .then(response => {
-//             return response.json();
-//         }).then(data => {
-//     let users = data.map((row) => {
-//     return <Row key={row.email} name={row.name} email={row.email}
-//     phone={row.phone} age={row.age}/>
-// });
-//         this.setState({users: users});
-//         console.log("state", this.state.users);
-//     });
-// }
-
-// render() {
-//     return (
-//         <table id="" className="highlight centered">
-//             <thead>
-//             <tr>
-//                 <th>Name</th>
-//                 <th>Email</th>
-//                 <th>Phone</th>
-//                 <th>Age</th>
-//                 <th>Delete</th>
-//             </tr>
-//             </thead>
-//             <tbody id="table_body">
-//             {this.state.users}
-//             </tbody>
-//         </table>
-//     );
-// }
-// }
-
-// export default Table;
