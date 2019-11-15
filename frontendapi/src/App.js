20267b69c213863e8492de2791cf6e4caa3aac2b
NickMckenney <mckenney2001@gmail.com>
2019-11-15 09:11:07 -0500

    Added React MDL which provided the navbar

---
+++ frontendapi/src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./App.css";
+import Main from "./components/Main.js";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+function App() {
+  return (
+    <div className="mainPage">
+      <Layout>
+        <Header className="header-color" title="Government Funding" scroll>
+          <Navigation>
+            <a href="/">Home</a>
+            <a href="http://localhost:4000/AwardSpending">Award Names</a>
+            <a href="http://localhost:4000/congress">Congress Funding</a>
+            <a href="http://localhost:4000/AwardId">Award Ids</a>
+            <a href="http://localhost:4000/state">States</a>
+          </Navigation>
+        </Header>
+        <Drawer title="Government Funding" className="header-color">
+          <Navigation>
+            <a href="/">Home</a>
+            <a href="http://localhost:4000/AwardSpending">Award Names</a>
+            <a href="http://localhost:4000/congress">Congress Funding</a>
+            <a href="http://localhost:4000/AwardId">Award Ids</a>
+            <a href="http://localhost:4000/state">States</a>
+          </Navigation>
+        </Drawer>
+        <Content>
+          <div className="page-content" />
+          <Main />
+        </Content>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontendapi/src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "../App.css";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <h1 className="intro-home-page">
+          The Best API on the Statistics of Government Funding Across All States
+          and Agencies Nation-Wide
+        </h1>
+      </div>
+    );
+  }
+}
+export default Home;
