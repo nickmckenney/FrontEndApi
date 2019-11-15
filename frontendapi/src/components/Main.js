cec9493f28ffad6278a7b06ecd72a3bb3e117835
NickMckenney <mckenney2001@gmail.com>
2019-11-15 09:51:01 -0500

    added state componenet in componenets

--- frontendapi/src/components/Main.js
+++ frontendapi/src/components/Main.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./Home.js";
+import State from "./State.js";
 
 const Main = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/state" component={State} />
   </Switch>
 );
 
---
+++ frontendapi/src/components/State.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "../App.css";
+
+class State extends Component {
+  render() {
+    return (
+      <div className="state">
+        <h1 className="intro-state-page">aaaaa</h1>
+      </div>
+    );
+  }
+}
+export default State;
